82b9290545a646d55206c5a770d2f09eca91903d
Gaetan SENN <gaetan.senn@gmail.com>
2019-10-05 18:51:38 +0200

    static/i18n: Add locales

---
+++ static/i18n/en.js
@@ -0,0 +1,23 @@
+export default {
+  header: {
+    nav: {
+      home: 'Home'
+    }
+  },
+  home: {
+    title: {
+      main: 'Hello',
+      sub: 'I am',
+      firstname: 'Gaetan SENN',
+      developer: 'Developer',
+      company: 'Dewib'
+    },
+    about: {
+      title: 'About me',
+      skills: 'Freelance JavaScript Developer • Vue.js / Nuxt.js • Node.js',
+      'description-1': 'I am',
+      'description-2': 'Gaetan SENN',
+      'description-3': 'Ut eu blandit lectus. Nunc vestibulum elit non ligula tincidunt dictum. Quisque nec dignissim enim. Nulla facilisi. Nullam aliquam lacus quis tellus dapibus maximus. In in turpis sit amet enim vehicula aliquam. Nulla sollicitudin non enim quis faucibus. In hac habitasse platea dictumst. Duis luctus metus et nibh rutrum, et maximus magna porta. Nulla suscipit urna vitae felis hendrerit, sed euismod lacus malesuada. Mauris nec tortor at nibh mattis dignissim at non sapien. In volutpat, felis ac hendrerit commodo, justo risus aliquet eros, finibus tristique est nulla elementum ligula.'
+    }
+  }
+}
---
+++ static/i18n/fr.js
@@ -0,0 +1,20 @@
+export default {
+  header: {
+    nav: {
+      home: 'Accueil'
+    }
+  },
+  home: {
+    title: {
+      main: 'Bonjour',
+      sub: 'Je suis',
+      firstname: 'Gaetan SENN',
+      developer: 'Développeur',
+      company: 'Dewib'
+    },
+    about: {
+      title: 'A propos de moi',
+      skills: 'Développeur Freelance JavaScript • Vue.js / Nuxt.js • Node.js'
+    }
+  }
+}
