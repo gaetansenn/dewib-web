e4a31256a33342b2209de86eea664b02d21ba3b7
Gaetan SENN <gaetan.senn@gmail.com>
2019-10-13 21:53:10 +0200

    components/base: Create heading component

---
+++ components/base/heading.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  functional: true,
+  props: {
+    attribute: {
+      type: String,
+      default: 'h1'
+    }
+  },
+  render (h, context) {
+    return h(context.props.attribute, {
+      class: {
+        heading: true,
+        [`${context.data.staticClass}`]: true
+      }
+    }, context.children)
+  }
+}
+</script>
+
+<style lang="scss">
+.heading {
+  @apply relative inline-block z-10;
+
+  &:after {
+    @apply absolute w-full left-0;
+    content: '';
+    height: 30px;
+    background-image: url('~assets/images/dots.png');
+    background-repeat: repeat;
+    z-index: -1;
+    bottom: -10px;
+  }
+}
+</style>
